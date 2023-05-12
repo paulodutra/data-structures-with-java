d93673f830ab7e0b7e85e85c44a78253c799ad8a
paulodutra <paulodutra.info@gmail.com>
2023-05-12 15:13:48 -0300

    feat: using the dynamic vector

--- src/VectorImplementation.java
+++ src/VectorImplementation.java
@@ -1,3 +1,20 @@
-public class Vector {
-    public static void
+import java.util.Vector;
+
+public class VectorImplementation {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        arr[0] = 1;
+
+//        Vector v = new Vector<>();
+        Vector v = new Vector<>(2, 3);
+        System.out.println(v.capacity());
+        v.addElement(1);
+        v.addElement("a");
+        System.out.println(v.size());
+        v.addElement("b");
+        System.out.println(v.capacity());
+//        while(v.iterator().hasNext()) {
+//            System.out.println(v.iterator().next());
+//        }
+    }
 }
